8bec792c691e6d5c7ee33087e7f6de6486a58ceb
Lauris Labanaukas <47822763+LaurisProject@users.noreply.github.com>
2020-06-17 10:27:34 +0300

    Movement, Jump

---
+++ SaveTheQueen/Assets/Scripts/Grounded.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grounded : MonoBehaviour
+{
+    GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = gameObject.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Ground")
+        {
+            Player.GetComponent<Movement>().isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            Player.GetComponent<Movement>().isGrounded = false;
+        }
+    }
+}
