121d94dc6e992847d6535cd5c58fbad58ca1714c
Lauris Labanaukas <47822763+LaurisProject@users.noreply.github.com>
2020-06-17 12:49:25 +0300

    v

--- SaveTheQueen/Assets/Scripts/Grounded.cs
+++ SaveTheQueen/Assets/Scripts/Grounded.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Grounded : MonoBehaviour
 {
-
     GameObject Player;
+
     // Start is called before the first frame update
     void Start()
     {
--- SaveTheQueen/Assets/Scripts/Movement.cs
+++ SaveTheQueen/Assets/Scripts/Movement.cs
@@ -6,27 +6,29 @@ public class Movement : MonoBehaviour
 {
     public float playerSpeed = 5f;
     public float jumpHight = 5f;
+
     public bool isGrounded = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Jump();
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.position += movement * Time.deltaTime * playerSpeed;
+            Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+            transform.position += movement * Time.deltaTime * playerSpeed;
     }
 
     void Jump()
     {
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if(Input.GetButtonDown("Jump") && isGrounded)
         {
-            GetComponent<Rigidbody>().AddForce(new Vector3(0f, jumpHight, 0f), ForceMode.Impulse);
+            gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0f, jumpHight, 0f), ForceMode.Impulse);
         }
     }
 }
